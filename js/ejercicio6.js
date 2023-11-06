00136ded1a09ab531b2e03db87f03784e6ba9b52
Julio Sanchez <jgs.sanchez94@gmail.com>
2023-11-05 23:44:32 -0300

    ejercicio6 listo

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,13 +1,13 @@
 let numero = prompt("Introduce un número para generar la pirámide:");
 
 
-    if (numero > 50) {
-        alert("El número no puede ser mayor que 50.");
+    if (numero > 30) {
+        alert("El número no puede ser mayor que 30");
     } else {
         for (let i = 1; i <= numero; i++) {
             let linea = "";
-            for (let l = 1; l <= i; l++) {
-                linea += l;
+            for (let j = 1; j <= i; j++) {
+                linea += i;
             }
             console.log(linea);
         }
